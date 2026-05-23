f566c128bdbe97bf422aed7d4254b9651c361915
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2026-05-23 22:29:29 +0530

    aaded the code of finding the ood and even numbers

--- index.py
+++ index.py
@@ -35,3 +35,9 @@ for i in range(1, 11):
         
 for i in range(2, 21):
     generateTable(i)
+
+for i in range(1, 11):
+    if(i * 2 == 0):
+        print("This is an even number)
+    else:
+        return "This is an odd"
