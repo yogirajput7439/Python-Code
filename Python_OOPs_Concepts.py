5ae71658dadf554419e04f335d0b7cd2d3e0cfe4
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2026-05-23 22:36:15 +0530

    Add Students class with example instantiation

--- Python_OOPs_Concepts.py
+++ Python_OOPs_Concepts.py
@@ -1,1 +1,12 @@
 # from module import myFunc
+
+# Class Is like a blueprint of car and 
+# Objects is Like Real Car
+
+class Students:
+  name = "Yogesh Rajput"
+  Age = 21
+
+  s1 = Students()
+
+print(s1.name) #Output --> Yogesh Rajput
