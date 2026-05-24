cc23d1c2956562f56f54c81892bcb0fb4dc25169
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2026-05-24 22:06:41 +0530

    Add numpy import and array creation examples

--- Easy_Python.py
+++ Easy_Python.py
@@ -1,1 +1,12 @@
+# Numpy In Python
 
+import Numpy as np # Import the numpy to use numpy features
+
+# 1. Array Creation 
+
+arr1 = np.array([1, 2, 3, 4, 5, 6]) # 1D Array
+
+arr2 = np.array([   
+  [1, 2, 3],
+  [4, 5, 6]
+])  # 2D Array 
