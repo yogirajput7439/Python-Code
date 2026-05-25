2a929d03550b8c74b26c5c6801ef652e8b789920
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2026-05-25 22:22:23 +0530

    Add array information print statements

--- Easy_Python.py
+++ Easy_Python.py
@@ -10,3 +10,10 @@ arr2 = np.array([
   [1, 2, 3],
   [4, 5, 6]
 ])  # 2D Array 
+
+# ARRAY INFORMATION
+
+print("Array Shape : ", arr.shape)
+print("Dimentions : ", arr.ndim)
+print("Size : ", arr.size)
+print("Data Types : ", arr.dtypes)
