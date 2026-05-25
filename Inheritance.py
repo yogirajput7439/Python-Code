ba5d462d3ef012996d6bbbb62faab2850b569292
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2026-05-25 22:31:18 +0530

    Add Animal and Dog classes with sound method

--- Inheritance.py
+++ Inheritance.py
@@ -1,3 +1,9 @@
-1	NCAB020365	2024015200374904	AAKKA,CHIMA,PAWARA	08	Vivekanand,Arts,,Sardar,Dalipsingh,Commerce,and,Science,College,,Chhatrapati,Sambhajinagar
-2	NCAB020366	2024015200374885	AARTI,SANTOSH,RAUT	08	Vivekanand,Arts,,Sardar,Dalipsingh,Commerce,and,Science,College,,Chhatrapati,Sambhajinagar
-3	NCAB020367	2024015200376315	AARYANI,INDRAJEET,PARVE	08	Vivekanand,Arts,,Sardar,Dalipsingh,Commerce,and,Science,College,,Chhatrapati,Sambhajinagar
+class Animal:
+  def sound(self):
+    print("Animal makes sound")
+
+class Dog(Animal):
+  pass
+
+d1 = Dog()
+d1.sound()
