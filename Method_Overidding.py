752bfd75c0007c059d24cfab9c00770276d31591
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2026-05-30 22:00:48 +0530

    Implement method overriding in Animal and Dog classes

---
+++ Method_Overidding.py
@@ -0,0 +1,14 @@
+class Animal:
+
+  def sound(self):
+    print("Animal sound")
+
+class Dog(Animal):
+  def sound(self):
+    print("Dog Barks")
+
+d1 = Dog() 
+
+dq.sound()
+
+# Output --> Dog Barks
