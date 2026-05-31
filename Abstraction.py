81cfcc8aa438782fea8807531677f876f6e954f6
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2026-05-31 21:52:54 +0530

    Implement abstract class Vehicle and subclass Car

--- Abstraction.py
+++ Abstraction.py
@@ -1,1 +1,19 @@
 # ABSTRACTION IN PYTHON
+
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+
+  @abstractmethod
+  def start(self):
+    pass
+
+class Car(Vehicle):
+
+  def Start(self):
+
+    print("Car Started")
+
+c1 = Car()
+c1.start()
+  
