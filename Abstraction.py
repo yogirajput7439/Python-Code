126cb9c479d05b1ca9fdf9847e119e1c40b270f2
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2026-07-13 23:29:17 +0530

    example in abstraction

--- Abstraction.py
+++ Abstraction.py
@@ -17,3 +17,26 @@ class Car(Vehicle):
 c1 = Car()
 c1.start()
   
+# example
+
+from abc import ABC, abstractmethod
+
+# Abstract class
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+    def eat(self):
+        print("Animal is eating")
+
+# Derived class
+class Dog(Animal):
+
+    def sound(self):
+        print("Dog barks")
+
+d = Dog()
+d.sound()
+d.eat()
